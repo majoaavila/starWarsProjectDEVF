77a457ac584422e2ebd2ab1273d50a86d84cd9a7
majoaavila <mavila7@ucol.mx>
2022-07-27 19:45:44 -0500

    update

--- js/main.js
+++ js/main.js
@@ -115,26 +115,3 @@ function toOrderCharacters() {
 }
 
 getCharacters();
-
-// async function getCharacters() {
-//     let url = 'https://akabab.github.io/starwars-api/api/all.json'
-//     let element = document.getElementById('app');
-//     // let characters = [];
-//     // const characters =  awaitfetch(url) 
-//     // .then(res => res.json())
-//     // .then(data => {
-//     //     console.log('respuestas server', data)
-//     //     characters = data
-//     // })
-//     const data = await fetch(url);
-//     let characters = await data.json();
-
-//     character = new Character(characters[0].name,characters[0].image)
-
-//     element.innerHTML = `
-//     <p>${character.getName()}</p>
-    
-//     <p><img src=${character.getImage()} height='400' width='350' /></p>`
-// }
-
-// getCharacters()
